2a01e8eb4531c270022ded490a4f43034cfaff79
Michael Martsynovskyi <mmartsn@gmail.com>
2019-08-06 14:19:14 +0300

    initial branch my_unit_02 commit

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,31 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const port = 3000;
-const hostname = 'localhost';
-module.exports = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
-}).listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+
+// Приложение выдает ответ “Hello from Express!” на запросы,
+// адресованные корневому URL (/) или маршруту.
+app.get('/', (request, response) => {
+    response.send('Hello from Express!');
+  });
+
+const server = app.listen(port, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log(`Server running at http://${host}:${port}/`);
 });
+
+
+
+//Можно и так по джаваскриптовски, по объектовски:
+//const options = {
+//  host: 'localhost',
+ //   port: 3000,
+ //}
+ //
+ //const server = app.listen(options, () => {
+ //   const host = server.address().address;
+ //   const port = server.address().port;
+ //   console.log(`Server running at ${options.host}:${options.port}/`);
+ //});
+ 
+
