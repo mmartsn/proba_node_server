5b55b83cfe76b86ffd09ec5218e3da00f83c8d19
Michael Martsynovskyi <mmartsn@gmail.com>
2019-08-08 01:53:41 +0300

    finished my_unit_02

--- bin/server.js
+++ bin/server.js
@@ -1,31 +1,60 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
+require('dotenv').config();
 
-// Приложение выдает ответ “Hello from Express!” на запросы,
-// адресованные корневому URL (/) или маршруту.
-app.get('/', (request, response) => {
-    response.send('Hello from Express!');
-  });
+// ========================================
+const APP_PORT =
+(process.env.NODE_ENV === 'test' ? process.env.TEST_APP_PORT : process.env.APP_PORT) || process.env.PORT || '3000';
+const APP_HOST = process.env.APP_HOST || '0.0.0.0';
+const APP_ENV = process.env.APP_ENV || 'development';
 
-const server = app.listen(port, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log(`Server running at http://${host}:${port}/`);
+// ========================================
+
+app.set('port', APP_PORT);
+app.set('host', APP_HOST);
+app.set('env', APP_ENV);
+
+// ========================================
+app.locals.title = process.env.APP_NAME || 'Мое приложение';
+app.locals.version = process.env.APP_VERSION;
+app.locals.email = process.env.APP_EMAIL || 'me@myapp.com';
+
+console.dir (app.locals.title)
+// => «Мое приложение»
+
+console.dir (app.locals.email)
+// => 'me@myapp.com'
+
+// ========================================
+app.use('/static', express.static(__dirname + '/../public/assets'));
+
+app.get("/", function(request, response){
+    response.sendFile(path.join(__dirname + '/../public/index.html'));
+});
+
+app.get("/about", function(request, response){
+    response.sendFile(path.join(__dirname + '/../public/about.html'));
+});
+
+app.get("/contact", function(request, response){
+    response.sendFile(path.join(__dirname + '/../public/contact.html'));
+});
+
+app.get('*', function(request, response){
+    response.status(404).send('What you want from me???');
 });
 
 
 
-//Можно и так по джаваскриптовски, по объектовски:
-//const options = {
-//  host: 'localhost',
- //   port: 3000,
- //}
- //
- //const server = app.listen(options, () => {
- //   const host = server.address().address;
- //   const port = server.address().port;
- //   console.log(`Server running at ${options.host}:${options.port}/`);
- //});
- 
+  app.listen(app.get('port'), app.get('host'), (err) => {
+    if (err) {
+      return console.log('something bad happened', err);
+    }
+    console.log(`${app.locals.title} Version: ${app.locals.version} started at http://${app.get('host')}:${app.get('port')}`);
+});
 
+// module.exports = app;
+
+
+  
